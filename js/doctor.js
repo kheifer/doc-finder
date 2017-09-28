846e5fce80e0c33a03fba4cccd67c8b722da5cc5
Kheifer Fuller <kheifer.fuller@gmail.com>
2017-09-27 18:49:39 -0700

    add doctorFinder method

--- js/doctor.js
+++ js/doctor.js
@@ -2,5 +2,25 @@ var apiKey = require('./../.env').apiKey;
 
 export let Doctor = {
 
-  getDoctorsByIssue:(query) =>{},
-};
+  doctorFInder: (issue) => {
+    $.ajax({
+     url: `https://api.betterdoctor.com/2016-03-01/doctors`,
+     type: 'GET',
+     data: {
+          query: issue,
+          location: newLocation,
+          sort: 'full-name-desc',
+          skip: 0,
+          limit: '20',
+          user_key: apiKey,
+          format: 'json'
+     },
+     success: function(response) {
+       console.log(response);
+  },
+     error: function(error) {
+      console.log(error);
+     }
+   });
+ },
+  };
